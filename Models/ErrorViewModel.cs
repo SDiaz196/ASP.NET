942ffba6291020ae018bdbedacb061ac13669e3e
Stephen Diaz <123036743+SDiaz196@users.noreply.github.com>
2023-05-08 12:22:34 -0400

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Session_Workshop.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
